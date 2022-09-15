5cf10deaba5c1d850f6ff986afb4699d00207ff3
krram712 <krram712@gmail.com>
2022-09-15 15:35:54 -0400

    initial commit

---
+++ src/com/sampleProject/MainClass.java
@@ -0,0 +1,12 @@
+package com.sampleProject;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudentData d=new StudentData();
+		Student s1=d.getStudentDetails();
+		System.out.println(s1);
+	}
+
+}
---
+++ src/com/sampleProject/StudentData.java
@@ -0,0 +1,11 @@
+package com.sampleProject;
+
+public class StudentData {
+	public Student getStudentDetails() {
+		Student s=new Student();
+		s.setId(1);
+		s.setName("raghu");
+		return s;
+	}
+
+}
